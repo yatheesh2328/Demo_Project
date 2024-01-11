972b68bf9f598e4526455b7915da4457782d431f
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-01-11 19:28:02 +0530

    Update 01_largestof2num.sh

--- 01_largestof2num.sh
+++ 01_largestof2num.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#read -p "ENTER THE VALUE OF A : " num1
-#read -p "ENTER THE VALUE OF B : " num2
+read -p "ENTER THE VALUE OF A : " num1
+read -p "ENTER THE VALUE OF B : " num2
 
 if [ $1 -eq $2 ]
 then
