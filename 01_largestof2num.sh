15f34bcb2c7171b72cbffb57f8c38d180f6583dc
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-01-11 19:56:08 +0530

    Update 01_largestof2num.sh

--- 01_largestof2num.sh
+++ 01_largestof2num.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #read -p "ENTER THE VALUE OF A : " num1
 #read -p "ENTER THE VALUE OF B : " num2
-num1=$1
-num2=$2
-if [ $1 -eq $2 ]
+if [ $num1 -eq $num2 ]
 then
 	echo "VALUE OF A AND B ARE EQUAL"
 else
