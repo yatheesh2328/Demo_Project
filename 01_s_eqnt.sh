41e4f51d1b15ca7a98295509ceb385080b002ce1
yatheesh2328 <154945628+yatheesh2328@users.noreply.github.com>
2024-01-12 13:29:53 +0530

    Update 01_s_eqnt.sh

--- 01_s_eqnt.sh
+++ 01_s_eqnt.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 echo "enter two numbers to check if they are equal ot not ? "
-
 read -p "Enter number A : " num1
 read -p "Enter number B : " num2
-
 #using if condition to verify if they are equal or not
 
 if [ $num1 -eq $num2 ]
